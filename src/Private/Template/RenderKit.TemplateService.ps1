85908ea8825bb23308e17ccbfc760bddc24d0318
djtroi <troismile02@gmail.com>
2026-02-19 13:16:45 +0100

    Enhance Confirm-Template function to validate template properties and add Test-FolderNode for folder validation

--- src/Private/Template/RenderKit.TemplateService.ps1
+++ src/Private/Template/RenderKit.TemplateService.ps1
@@ -24,8 +24,49 @@ function Get-ProjectTemplate {
     return $json
 }
 
-function Confirm-Template {}
+function Confirm-Template {
     param(
-        
+        [Parameter(Mandatory)]
+        $Template
     )
+
+    if (!($Template.Version)) {
+        Write-RenderKitLog -Level Error -Message "Template is missing 'Version' property"
+    }
+
+    if (!($Template.Name)) {
+        Write-RenderKitLog -Level Error -Message "Template is missing 'Name' property."
+    }
+    if (!($Template.Folders)) {
+        Write-RenderKitLog -Level Error -Message "Template is Mssing 'Folders' property."
+    }
+
+    if (!($Template.Version -ne "1.0")) { #TODO Implement Logic, that reads from .psd1 the actual schema version 
+        Write-RenderKitLog -Level Warning -Message "Unsupported Template Version '$($Template.Version)'."
+    }
+
+    foreach ($folder in $Template.Folders){
+        Test-FolderNode $folder
+    }
+}
+
+function Test-FolderNode {
+    param(
+        [Parameter(Mandatory)]
+        $Folder
+    )
+
+    if (!($Folder.Name)) {
+        Write-RenderKitLog -Level Error -Message "Folder node missing 'Name' Property"
+    }
+
+    if (!($Folder.PSObject.Properties.Name -contains "SubFolders")) {
+        Write-RenderKitLog "Folder '$($Folder.Name)' missing 'SubFolders' property"
+    }
+
+    if($Folder.SubFolders) {
+        foreach ($sub in $Folder.SubFolders) {
+            Test-FolderNode $sub
+        }
+    }
 }
